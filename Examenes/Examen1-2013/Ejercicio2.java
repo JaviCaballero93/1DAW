703c5e5236e703002c66943f19e26f5197e47061
JaviCaballero93 <javiercaballero93@gmail.com>
2016-02-20 08:03:59 +0100

    Rename Examenes/Ejercicio2.java to Examenes/Examen1-2013/Ejercicio2.java

--- Examenes/Examen1-2013/Ejercicio2.java
+++ Examenes/Examen1-2013/Ejercicio2.java
@@ -12,7 +12,7 @@ public class Ejercicio2 {
 		
 		for(int i=0; i<matriz.length; i++) {
 			for(int j=0; j<matriz[i].length; j++) {
-				matriz[i][j]= Integer.parseInt(JOptionPane.showInputDialog("Introduce un valor para la posición " + i + j));
+				matriz[i][j]= Integer.parseInt(JOptionPane.showInputDialog("Introduce un valor para la posiciÃ³n " + i + j));
 			}
 		}
 		
