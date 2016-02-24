86d39486747225aeccdb35282bc07168f0eb88df
Javier Caballero Espina <javiercaballero93@gmail.com>
2016-02-24 03:59:45 +0100

    Update ColaTest.java

--- Examenes/Examen2-2014/Ejercicio2/ColaTest.java
+++ Examenes/Examen2-2014/Ejercicio2/ColaTest.java
@@ -1,5 +1,28 @@
+import javax.swing.JOptionPane;
+
 public class ColaTest() {
-        
-        
+        public static void main(String[] args) {
+                
+                Cola cola1 = new Cola();
+                
+                cola1.colocar(new Persona("Pepe", 1));
+                cola1.colocar(new Persona("Javi", 0));
+                cola1.colocar(new Persona("Maria", 2));
+		cola1.colocar(new Persona("Jesus", 0));
+		cola1.colocar(new Persona("Ana", 1));
+		cola1.colocar(new Persona("Juan", 2));
+		cola1.colocar(new Persona("Irene", 3));
+		
+		JOptionPane.showMessageDialog(cola1.listar());
+		
+		Cola cola2 = new Cola();
+		
+	        cola2.colocar(new Persona("Ignacio", 2));
+		cola2.colocar(new Persona("Tońi", 0));
+		cola2.colocar(new Persona("Docarmo", 1));
+		
+		cola1.mezclar(cola2);
+		
+		JOptionPane.showMessageDialog(cola1);
+        }
 }
- 
